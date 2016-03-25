47d348c88ec49f304596c294054d0056901de4c6
Joe Natale <jpnatale@gmail.com>
2016-03-25 09:17:31 -0400

    post todos1

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 var PORT = process.env.PORT || 3000
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Do laundry',
-	completed: true
-}]
+var todoNextId = 1
+
+var todos = []
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res){
 	res.send('Todo API Root')
@@ -42,7 +35,23 @@ app.get('/todos/:id', function (req, res){
 	}
 })
 
-// GET /todos/id
+// POST /todos/
+app.post('/todos', function (req, res){
+	var body = req.body
+	body.id = todoNextId
+	todoNextId++
+	todos.push(body)
+
+
+	// console.log('description: ' + body.description)
+
+	res.json(body)
+
+	console.log(todos)
+
+
+})
+
 
 app.listen(PORT, function(){
 
