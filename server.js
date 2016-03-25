5ff34acacd3f6af3456d6449c5f4707d6a2a5e0a
Joe Natale <jpnatale@gmail.com>
2016-03-25 13:56:29 -0400

    put todos

--- server.js
+++ server.js
@@ -55,11 +55,36 @@ app.delete('/todos/:id', function (req, res){
 		res.status(404).json({"error": "no todo found with that id"})
 	} else {
 	todos = _.without(todos,matchedIdTodo)
-
 	res.json(todos)
 	}
-
 })
+
+app.put('/todos/:id', function (req, res){
+	var todoId = parseInt(req.params.id, 10)
+	var matchedIdTodo = _.findWhere(todos, {id: todoId})
+	var body = _.pick(req.body, 'description', 'completed')
+	var validAttributes = {}
+
+	if (!matchedIdTodo){
+		res.status(404).json({"error": "no todo found with that id"})
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send()
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send()
+	}
+
+	_.extend(matchedIdTodo, validAttributes)
+	res.json(matchedIdTodo)
+})
+
 
 
 app.listen(PORT, function(){
