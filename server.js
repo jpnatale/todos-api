22e14565117819360de9814dc8d4538d8b3063a0
Joe Natale <jpnatale@gmail.com>
2016-03-28 10:45:35 -0400

    Add postgress for production

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.put('/todos/:id', function(req, res) {
 	_.extend(matchedIdTodo, validAttributes)
 	res.json(matchedIdTodo)
 })
-//test2
+
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log("Express server listening on port " + PORT + "!")
