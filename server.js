5ab735a7d4f20082df6e68ce7635693f40703df5
Joe Natale <jpnatale@gmail.com>
2016-03-28 07:52:33 -0400

    add server stuff

--- server.js
+++ server.js
@@ -46,40 +46,28 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10)
-	var matchedIdTodo = _.findWhere(todos, {
-		id: todoId
+
+	db.todo.findById(todoId).then(function (todo){
+		if (!!todo) {
+			res.json(todo.toJSON())
+		} else {
+			res.status(404).send()
+		}
+	}, function(e){
+		res.status(500).send()
 	})
-
-	if (typeof matchedIdTodo == 'undefined') {
-
-		res.status(404).send()
-	} else {
-		res.json(matchedIdTodo)
-	}
 })
 
 // POST /todos/
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed')
 
-db.todo.create(body).then(function(todo){
-	res.json(todo.toJSON())
-}, function (e){
-	res.status(400).json(e)
-})
+	db.todo.create(body).then(function(todo) {
+		res.json(todo.toJSON())
+	}, function(e) {
+		res.status(400).json(e)
+	})
 
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-
-	// 	return res.status(400).send();
-	// }
-	// body.description = body.description.trim()
-
-	// body.id = todoNextId
-	// todoNextId++
-	// todos.push(body)
-
-	// res.json(body)
 })
 
 app.delete('/todos/:id', function(req, res) {
