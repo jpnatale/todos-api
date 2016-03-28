a588e539fd033b9ce2e7354d8bae7b016aa966e9
Joe Natale <jpnatale@gmail.com>
2016-03-28 11:36:21 -0400

    Add delete and put functionality

--- server.js
+++ server.js
@@ -65,48 +65,51 @@ app.post('/todos', function(req, res) {
 
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10)
-	var matchedIdTodo = _.findWhere(todos, {
-		id: todoId
+		db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted){
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			})
+		} else {
+			res.status(204).send()
+		}
+	}, function (){
+		res.statsu(500).send()
 	})
-
-	if (!matchedIdTodo) {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		})
-	} else {
-		todos = _.without(todos, matchedIdTodo)
-		res.json(todos)
-	}
 })
 
 app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10)
-	var matchedIdTodo = _.findWhere(todos, {
-		id: todoId
-	})
+
 	var body = _.pick(req.body, 'description', 'completed')
-	var validAttributes = {}
+	var attributes = {}
 
-	if (!matchedIdTodo) {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		})
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed
+	} 
+
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description
 	}
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send()
-	}
+	db.todo.findById(todoId).then(function (todo){
+		if (todo) {
+			return todo.update(attributes)
+		} else {
+			res.status(404).send()
+		}
+	},function (){
+		res.status(500).send()
+	}).then(function (todo) {
+		res.json(todo.toJSON())
+	}, function (e){
+		res.status(400).json(e)
+	})
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send()
-	}
-
-	_.extend(matchedIdTodo, validAttributes)
-	res.json(matchedIdTodo)
 })
 
 db.sequelize.sync().then(function() {
